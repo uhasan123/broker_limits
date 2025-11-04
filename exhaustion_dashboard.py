735661221da2c9f8f67701cef046248aa2ca3d99
Usman Hasan <uhasan@bobtail.com>
2025-11-05 00:39:17 +0500

    testing streamlit app with exhaustion counter

---
+++ exhaustion_dashboard.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import streamlit as st
+
+from broker_report import broker_report
+
+def get_exhausted_debtors():
+    obj=broker_report()
+    conn=obj.make_db_connection()
+    query="select count(distinct id) from debtors d where d.approved_total>=d.debtor_limit and d.status = 'active' and d.debtor_limit<>100"
+    exhaustion_count=pd.read_sql_query(query, conn)
+    return exhaustion_count
+
+tab1, tab2, tab3=st.tabs(['TAB 1', 'TAB 2', 'TAB 3'])
+with tab1:
+    brokers_exhausted=get_exhausted_debtors()
+    st.write('Exhaustion counter', brokers_exhausted)
+        
