278baf132bd4c1305c5f7378bb36cbe0c85419c6
Usman Hasan <uhasan@bobtail.com>
2025-11-05 00:49:22 +0500

    update exhaustion_dashborad.py

--- exhaustion_dashboard.py
+++ exhaustion_dashboard.py
@@ -7,8 +7,9 @@ from broker_report import broker_report
 def get_exhausted_debtors():
     obj=broker_report()
     conn=obj.make_db_connection()
-    query="select count(distinct id) from debtors d where d.approved_total>=d.debtor_limit and d.status = 'active' and d.debtor_limit<>100"
+    query="select count(distinct id) as exhasuted_brokers from debtors d where d.approved_total>=d.debtor_limit and d.status = 'active' and d.debtor_limit<>100"
     exhaustion_count=pd.read_sql_query(query, conn)
+    exhaustion_count=exhaustion_count['exhasuted_brokers'].iloc[0]
     return exhaustion_count
 
 tab1, tab2, tab3=st.tabs(['TAB 1', 'TAB 2', 'TAB 3'])
