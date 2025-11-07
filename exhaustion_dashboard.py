8e5e618eea456abb052653d396071779536fe2b4
Usman Hasan <uhasan@bobtail.com>
2025-11-07 16:58:58 +0500

    resetting index for debtor wise info in tab1

--- exhaustion_dashboard.py
+++ exhaustion_dashboard.py
@@ -202,7 +202,7 @@ with tab1:
         colss=st.columns(2)
         colss[0].write(ageing_cohort_df)
         colss[1].write(limit_cohort_df)
-        st.write(debtor_level.sort_values(by='utilization_rate', ascending=False).reset_index())
+        st.write(debtor_level.sort_values(by='utilization_rate', ascending=False).reset_index().drop('index', axis=1))
         st.session_state.tab1=False
 
 with tab2:
