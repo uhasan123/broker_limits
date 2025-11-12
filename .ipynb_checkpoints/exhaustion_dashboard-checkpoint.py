fd5cd8ce50d9ba9256350c1b003f107cc8fa228b
Usman Hasan <uhasan@bobtail.com>
2025-11-12 16:45:20 +0500

    pivot table adjustment

--- .ipynb_checkpoints/exhaustion_dashboard-checkpoint.py
+++ .ipynb_checkpoints/exhaustion_dashboard-checkpoint.py
@@ -374,7 +374,7 @@ with tab3:
             pivot_table, df_t, pivot_table_client_conc=broker_report.generate_report(broker_level_df, broker_profile_report=True, cohort=value,payment_trend_count=5, payment_trend_step='default', debtors_df=debtors_df, brokers_df=brokers_df, invoice_df=invoice_df)
             st.write('Debtors Info')
             st.write(df_t)
-            cols_=st.columns([2,2,2])
+            cols_=st.columns([3,2,2])
             cols_[0].write('Metrics Averages and Standard Deviation')
             pivot_table=pivot_table.reset_index()
             pivot_table.columns = [col[0] if col[0] != '' else col[1] for col in pivot_table.columns]
@@ -382,6 +382,7 @@ with tab3:
             cols_[1].write('Client Concentration')
             pivot_table_client_conc=pivot_table_client_conc.reset_index()
             pivot_table_client_conc.columns = [col[0] if col[0] != '' else col[1] for col in pivot_table_client_conc.columns]
+            pivot_table_client_conc=pivot_table_client_conc.rename(columns={'m':'metrics', 'l':'count'})
             cols_[1].write(pivot_table_client_conc)
 
     st.markdown(f"<h1 style='font-size:28px; color:green;'>Broker Payment Trend</h1>", unsafe_allow_html=True)
