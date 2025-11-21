85f21a761a766c0f23fefd4a650e391fafdbe636
uhasan123 <uhasan@bobtail.com>
2025-11-21 15:08:21 +0500

    Comment out google_credentials definition
    Comment out the google_credentials dictionary definition.

--- exhaustion_dashboard_2.py
+++ exhaustion_dashboard_2.py
@@ -35,7 +35,7 @@ st.title("Exhaustion Monitoring Dashboard")
 # client_x509_cert_url=os.getenv('client_x509_cert_url')
 # universe_domain=os.getenv('universe_domain')
 
-google_credentials={'type':typee, 'project_id':project_id, 'private_key_id':private_key_id, 'private_key':private_key, 'client_email':client_email, 'client_id':client_id, 'auth_uri':auth_uri, 'token_uri':token_uri, 'auth_provider_x509_cert_url':auth_provider_x509_cert_url, 'client_x509_cert_url':client_x509_cert_url, 'universe_domain':universe_domain}
+# google_credentials={'type':typee, 'project_id':project_id, 'private_key_id':private_key_id, 'private_key':private_key, 'client_email':client_email, 'client_id':client_id, 'auth_uri':auth_uri, 'token_uri':token_uri, 'auth_provider_x509_cert_url':auth_provider_x509_cert_url, 'client_x509_cert_url':client_x509_cert_url, 'universe_domain':universe_domain}
 
 # st.write(google_credentials)
 
