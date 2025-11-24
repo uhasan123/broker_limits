c95d80f38ac779865270eebc07bd4b9a72e12c36
Usman Hasan <uhasan@bobtail.com>
2025-11-24 15:00:22 +0500

    removing local scheduling in the script

--- upload_to_sheets.py
+++ upload_to_sheets.py
@@ -28,27 +28,27 @@ on a.id=b.debtor_id'''
     exhaust_debtors=pd.read_sql_query(query, conn)
     return exhaust_debtors
 
-def job():
-    SPREADSHEET_NAME = 'Sample'
-    SHEET_NAME = 'Sheet1'
-    CREDENTIALS_FILE = './credentials.json'
+# def job():
+SPREADSHEET_NAME = 'Sample'
+SHEET_NAME = 'Sheet1'
+CREDENTIALS_FILE = './credentials.json'
 
-    sheet_by_name = connect_to_gsheet(CREDENTIALS_FILE, SPREADSHEET_NAME, sheet_name=SHEET_NAME)
+sheet_by_name = connect_to_gsheet(CREDENTIALS_FILE, SPREADSHEET_NAME, sheet_name=SHEET_NAME)
 
-    # load_dotenv("config_ssh.env")
-    obj=broker_report()
-    conn=obj.make_db_connection()
-    conn.autocommit=True
+# load_dotenv("config_ssh.env")
+obj=broker_report()
+conn=obj.make_db_connection()
+conn.autocommit=True
 
-    exhausted_df=get_exhausted_debtors()
-    data_to_upload = [exhausted_df.columns.values.tolist()] + exhausted_df.values.tolist()
-    # data_to_upload
-    sheet_by_name.clear()
-    sheet_by_name.append_rows(data_to_upload)
-    print('uploaded')
+exhausted_df=get_exhausted_debtors()
+data_to_upload = [exhausted_df.columns.values.tolist()] + exhausted_df.values.tolist()
+# data_to_upload
+sheet_by_name.clear()
+sheet_by_name.append_rows(data_to_upload)
+print('uploaded')
     
-schedule.every().hour.do(job)
+# schedule.every().hour.do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60)
