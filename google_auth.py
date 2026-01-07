3034f37ae7577f9269dcd1500a00701c6cfa7be3
uhasan123 <uhasan@bobtail.com>
2026-01-07 16:25:58 +0500

    Update google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -63,7 +63,7 @@ def login():
   # auth_url, _ = flow.authorization_url(prompt="consent")
   # st.markdown(f"[🔐 Login with Google]({auth_url})")
 
-  query_params = st.experimental_get_query_params()
+  # query_params = st.experimental_get_query_params()
   query_params = st.query_params
   code = query_params.get("code")
   
